577df6392eae2c29f0461d24f4710caf06cc8cea
Kirill Zhukov <zklevsha@gmail.com>
2025-07-22 10:26:30 +0000

    refactor some SQL into macroses

--- models/date_demension.sql
+++ models/date_demension.sql
@@ -3,18 +3,8 @@ with CTE as (
         TO_TIMESTAMP(started_at) as started_at_ts,
         DATE(started_at_ts) as date_started_at,
         HOUR(started_at_ts) as hour_started_at,
-    
-        case
-            when DAYNAME(started_at_ts) in ('Sat', 'Sun') then 'WEEKEND'
-            else 'BUISNESSDAY'
-        end as type_of_day,
-
-        case 
-            when MONTH(started_at_ts) in (12,1,2) then 'WINTER'
-            when MONTH(started_at_ts) in (3,4,5) then 'SPRING'
-            when MONTH(started_at_ts) in (6,7,8) then 'SUMMER'
-            else 'AUTUM' 
-        end as station_of_year
+        {{ get_buisnes_or_weekend_day('started_at_ts') }} as type_of_day,
+        {{ get_season('started_at_ts') }} as station_of_year
     
     from {{ source('demo', 'bike') }}
     where started_at != 'started_at'
